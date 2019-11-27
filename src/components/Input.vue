26d60524b2a58c67674708b37cc0ee7dc5d3bd48
David Lizárraga <dlizarraga@emcor.mx>
2019-11-26 17:18:51 -0700

    inputs

---
+++ src/components/Input.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <label>{{name}}</label>
+    <input type="text" v-model="value">
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+
+@Component
+export default class Input extends Vue {
+  @Prop() name!: string;
+
+  private value: string = '';
+
+  private mounted() {
+    console.log(this.name);
+  }
+
+  @Watch('value')
+  private watchValue(value: string) {
+    this.$emit('input', value);
+    this.$emit('result', value);
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <Input name="Email" v-model="email" @result="result" type="email"/>
+    <Input name="Password" v-model="password" type="password"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+import Input from '@/components/Input.vue';
+
+@Component({
+    components: {
+        Input,
+    }
+})
+export default class Login extends Vue {
+    private email: string = '';
+    private password: string = '';
+
+    @Watch('email')
+    private watchValue() {
+        console.log('login', this.email);
+    }
+
+    @Watch('password')
+    private watchPassword() {
+        console.log('password', this.password);
+    }
+
+    private result(value: string) {
+        console.log('result', value);
+    }
+}
+</script>
+
+<style>
+
+</style>
